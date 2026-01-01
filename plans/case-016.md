f2cfa35cb6fe619cd3d32ab7b1dcb02e45093e45
Eval Generator <eval@example.com>
2026-01-01 00:00:00 +0000

    case-016

---
+++ plans/case-016.md
@@ -0,0 +1,4 @@
+# case-016
+
+File: `typescript/case-016/src/case.ts`
+Function: `review_case_6`
---
+++ typescript/case-016/src/case.ts
@@ -0,0 +1,4 @@
+export function review_case_6(values: number[]): number {
+  values.push(4);
+  return values.length;
+}
