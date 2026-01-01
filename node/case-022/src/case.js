c7442a0cb2342d7681871f500dcc93a2dba3d128
Eval Generator <eval@example.com>
2026-01-01 00:00:00 +0000

    case-022

---
+++ node/case-022/src/case.js
@@ -0,0 +1,5 @@
+function review_case_2(values) {
+  values.push(4);
+  return values.length;
+}
+module.exports = { review_case_2 };
---
+++ plans/case-022.md
@@ -0,0 +1,4 @@
+# case-022
+
+File: `node/case-022/src/case.js`
+Function: `review_case_2`
